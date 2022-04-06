62f0097e5dfa5645aa2aa1e9f334d70cc4d52b61
Kokito <ronny123bastidas@gmail.com>
2022-04-06 12:37:41 -0500

    print

---
+++ hentai.py
@@ -0,0 +1,22 @@
+print("hello world")
+print(3)
+
+matematicas="variable"
+print(matematicas)
+
+matematicas=345
+print(matematicas)
+
+matematicas=(789,"hello world")
+#tupla=es lo que esta en el parentesís y no se puede modificar
+print(matematicas)
+
+matematicas=["hello world",True,5]
+print(matematicas)
+
+diccionario={
+    'nombre':"Lucas"
+}
+print(diccionario)
+
+print(type)
